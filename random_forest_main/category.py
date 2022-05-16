2786eae45afa5f4b8b257c66b557bedbbf3e12ef
unknown <jeremiah_iibarreta@yahoo.com>
2022-05-16 13:33:22 +0800

    final category

--- random_forest_main/category.py
+++ random_forest_main/category.py
@@ -1,71 +1,102 @@
-
-
 # VARIABLES
 _DISTRIBUTION = { 'INTEREST': 44.80, 'LEARNING_STYLE' : 20.3, 'ACAD_ACHIEVMENTS': 34.90 }
 
-# CATEGORIES
 _VARIABLES = {
 
     "INTEREST" : {
-        "REALISTIC" : { "GAS": 100, "STEM": 93, "HUMSS": 45,  "ABM": 23 },
-        "INVESTIGATE" : { "STEM": 93, "ABM": 90, "HUMSS": 43, "GAS": 11 },
-        "ARTISTIC" : { "HUMSS": 82, "GAS": 38, "STEM": 27, "ABM": 13 },
-        "SOCIAL" : {  "HUMSS": 103, "GAS": 94, "STEM": 34,  "ABM": 16 },
-        "ENTERPRISING" : { "ABM": 120, "GAS": 36, "HUMSS": 30, "STEM": 27 },
-        "CONVENTIONAL" : { "ABM": 113, "STEM": 102, "GAS": 96,  "HUMSS": 80 }
+        "REALISTIC" : { "STEM": 4, "GAS": 3, "HUMSS": 2,  "ABM": 1 },
+        "INVESTIGATE" : { "STEM": 4, "ABM": 3, "HUMSS": 2, "GAS": 1 },
+        "ARTISTIC" : { "HUMSS": 4, "GAS": 3, "STEM": 2, "ABM": 1 },
+        "SOCIAL" : {  "HUMSS": 4, "GAS": 3, "STEM": 2,  "ABM": 1 },
+        "ENTERPRISING" : { "ABM": 4, "GAS": 3, "HUMSS": 2, "STEM": 1 },
+        "CONVENTIONAL" : { "ABM": 4, "STEM": 3, "GAS": 2,  "HUMSS": 1 }
     },
 
     "LEARNING_STYLE" : {
-        "LINGUISTIC" : { "HUMSS": 111, "ABM": 94, "GAS": 44, "STEM": 6 },
-        "LOGICAL" :  { "STEM": 98, "ABM": 90,  "HUMSS": 24,  "GAS": 23 },
-        "SPATIAL" :  { "ABM": 62, "STEM": 37, "HUMSS": 29, "GAS":  19 },
-        "BODILY" : { "HUMSS": 54, "GAS": 26, "STEM": 13, "ABM": 1 },
-        "MUSICAL" : { "GAS": 53, "HUMSS": 35, "STEM": 32, "ABM": 21 },
-        "INTERPERSONAL" : { "ABM": 110, "HUMSS": 64,"GAS": 31, "STEM": 30 },
-        "INTRAPERSONAL" : { "STEM": 94, "GAS": 84, "HUMSS": 34, "ABM": 18 },
-        "NATURALIST" : { "GAS": 92,"HUMSS": 49, "ABM": 34, "STEM": 27 }
+        "LINGUISTIC" : { "HUMSS": 4, "ABM": 3, "GAS": 2, "STEM": 1 },
+        "LOGICAL" :  { "STEM": 4, "ABM": 3,  "HUMSS": 2,  "GAS": 1 },
+        "SPATIAL" :  { "ABM": 4, "STEM": 3, "HUMSS": 2, "GAS":  1 },
+        "BODILY" : { "HUMSS": 4, "GAS": 3, "STEM": 2, "ABM": 1 },
+        "MUSICAL" : { "GAS": 4, "HUMSS": 3, "STEM": 2, "ABM": 1 },
+        "INTERPERSONAL" : { "ABM": 4, "HUMSS": 3,"GAS": 2, "STEM": 1 },
+        "INTRAPERSONAL" : { "STEM": 4, "GAS": 3, "HUMSS": 2, "ABM": 1 },
+        "NATURALIST" : { "GAS": 4,"HUMSS": 3, "ABM": 2, "STEM": 1 }
     },
 
 
     "ACAD_ACHIEVMENTS" : {
-        "MATH" :  { "STEM": 103, "ABM": 89, "GAS": 53, "HUMSS": 32},
-        "ENGLISH" : { "HUMSS": 93, "ABM": 82, "GAS": 36, "STEM": 29},
-        "SCIENCE" : { "STEM": 88, "GAS": 61, "HUMSS": 58, "ABM": 46},
-        "SOCIAL_SCIENCE"  : { "HUMSS":111, "ABM": 87, "GAS": 32, "STEM": 25 }
+        "MATH" :  { "STEM": 4, "ABM": 3, "GAS": 2, "HUMSS": 1},
+        "ENGLISH" : { "HUMSS": 4, "ABM": 3, "STEM": 2, "GAS": 1},
+        "SCIENCE" : { "STEM": 4, "GAS": 3,  "ABM": 2, "HUMSS": 1},
+        "SOCIAL_SCIENCE"  : { "HUMSS": 4, "ABM": 3, "GAS": 2, "STEM": 1 }
     }
    
 }
 
-# TOTAL RESPONDENTS
-_TOTAL_RESPONDENTS = 500
+def specialCases(data):
+    isOnyGrade = 0
+    for i in range(14):
+        isOnyGrade += data[i]
+    
+    if(isOnyGrade != 0):
+        return None
 
+    isAllSimilar = 1
+   
+    for i in range(14, len(data) - 1):
+        if(data[i] == data[i + 1]):
+            isAllSimilar += 1
+           
+    if(isAllSimilar == 1):
+        return None
+
+    if isAllSimilar == 4: 
+        return 'GAS'
+    
+    return None
+
+   
 
 #  https://www.statisticshowto.com/weighting-factor/
 def WeightFactorAlgorithm(data): 
-    print(data)
+   
     res = { "HUMSS": 0, "GAS": 0, "STEM": 0, "ABM": 0 }
-    data_counter = 0
-    for variable_id, variable_value in _VARIABLES.items():
-        categories = variable_value
-        for category_Id, category_value in categories.items():
-            strands = category_value
-            for strand_Id, strand_value in strands.items():
-                score = data[data_counter]
-                if(score > 0):
-                    if(variable_id != "ACAD_ACHIEVMENTS"):
-                        res[strand_Id] += ((strand_value / _TOTAL_RESPONDENTS) * 100) * score
-                    else:
-                        grade = strand_value
-                        res[strand_Id] +=  grade
-              
-            data_counter += 1   
 
+    spRes = specialCases(data)
+    if(spRes != None):
+        return spRes
+    else:
         
+        data_counter = 0
+        for variable_id, variable_value in _VARIABLES.items():
+                        
+
+            categories = variable_value
+            
+            temp_counter = { "HUMSS": 0, "GAS": 0, "STEM": 0, "ABM": 0 }
+            
+
+            for category_idx, category_value in categories.items():
+                strands = category_value
+                for strand_name, strand_value in strands.items():
+                    test_score = data[data_counter]
+                    if(test_score > 0):
+                        if(variable_id != "ACAD_ACHIEVMENTS"):
+                            temp_counter[strand_name] += ((strand_value) * test_score)
+                        else:
+                            temp_counter[strand_name] += (strand_value + test_score)
+               
+                data_counter += 1   
+            
+    
+            
+            for temp_counter_id, temp_counter_value in temp_counter.items():
+            
+                res[temp_counter_id] += temp_counter[temp_counter_id] * (_DISTRIBUTION[variable_id] / 100)
         
-        for temp_counter_id, temp_counter_value in res.items():
-            res[temp_counter_id] += res[temp_counter_id] * (_DISTRIBUTION[variable_id] / 100)
-      
+            
         
+    
 
     
     
@@ -76,28 +107,21 @@ def WeightFactorAlgorithm(data):
             tentativeStrand[0] = res_id
     
    
+    print("final Result: ", res)
     return tentativeStrand[0]
 
 
-# connect this function 
-#def getRecommendedTrack(data):
-#    weightFactor = WeightFactorAlgorithm(data)
-#    randomForest = predict_probabilities([data])[0][0]
-#    if(weightFactor != randomForest):
-#        return weightFactor
-#    return randomForest
 
 
 
-
-    # data = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 90, 90, 90, 90] 
+    # data = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 90, 90, 90, 90] # OUTPUT STEM ✔️
     # data = [3, 2, 3, 1, 4, 4, 4, 5, 5, 5, 6, 3, 5, 6, 86, 81, 84, 87] 
     # data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100, 90, 90, 90]
     # data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100, 90, 90, 90]
     # data = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 90, 90, 90, 90] 
     # data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100, 0, 0]    
     # data = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 90, 90, 90, 90] 
-    # data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100, 0, 0]  
+   
     # data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100, 0, 0, 0]  
     # data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100, 0]
     # data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100, 0]  
@@ -105,13 +129,15 @@ def WeightFactorAlgorithm(data):
     # data = [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0] 
     # data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
     # data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0]  
-    # data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0] 
-    # data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
-    # data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
-    # data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0]
-    # data = [3, 0, 3, 0, 0, 0, 3, 3, 0, 3, 0, 0, 3, 0, 90, 90, 90, 90]
-    # data = [0, 0, 4, 4, 0, 0, 4, 0, 0, 4, 0, 0, 0, 0, 90, 90, 90, 90]
-    # data = [0, 0, 0, 0, 4, 4, 0, 0, 4, 0, 0, 0, 4, 0, 90, 90, 90, 90]
+    # data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]     # OUTUPUT GAS ✔️ 
+    # data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]     # OUTPUT ABM ✔️
+    # data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0]     # OUTPUTSTEM ✔️
+    # data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0]     # OUTPUT HUMSS ✔️
+    # data = [3, 0, 3, 0, 0, 0, 3, 3, 0, 3, 0, 0, 3, 0, 90, 90, 90, 90] # OUTPUT HUMSS ✔️
+    # data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 75, 100,75, 75] # OUTPUT HUMSS ✔️
+    # data = [0, 0, 0, 0, 4, 4, 0, 0, 4, 0, 0, 0, 4, 0, 90, 90, 90, 90] # OUTPUT ABM  ✔️ 
+    # data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 75, 100,75, 75] # OUTPUT HUMS ✔️
+    # data = [0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 90, 90, 90, 90] # OUTPUT STEM ✔️
 
 
 
